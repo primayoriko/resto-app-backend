86c621ef6ec6d3022f8f8fa89741d75e050cb17f
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-03-15 16:44:04 +0700

    chore: add userprincipaldetailsservice class

--- src/main/java/com/future/restoapp/security/UserPrincipalDetailsService.java
+++ src/main/java/com/future/restoapp/security/UserPrincipalDetailsService.java
@@ -1,4 +1,27 @@
 package com.future.restoapp.security;
 
-public class UserPrincipalDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.future.restoapp.entity.User;
+import com.future.restoapp.repository.UserRepository;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(s);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
 }
