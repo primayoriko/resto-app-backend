bcc2833e9f72473a128d407e1cc2e0a5071431c4
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-03-29 18:34:17 +0700

    chore: setup UserService and template for UserServiceBean

--- src/main/java/com/future/restoapp/controller/UserController.java
+++ src/main/java/com/future/restoapp/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.future.restoapp.controller;
 
+import com.future.restoapp.base.BaseResponse;
+import com.future.restoapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,5 +11,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RequestMapping(value = UserControllerPath.BASE)
 @CrossOrigin
 public class UserController {
-    
+    @Autowired
+    private UserService userService;
+
+//    @RequestMapping()
+//    public BaseResponse create(){
+//
+//    }
 }
--- src/main/java/com/future/restoapp/controller/UserControllerPath.java
+++ src/main/java/com/future/restoapp/controller/UserControllerPath.java
@@ -7,6 +7,8 @@ public interface UserControllerPath {
     String BASE = UrlBasePath.CURRENT_CLIENT + "/users";
     String REGISTER = BASE + "/register";
     String UPDATE = BASE;
+    String FETCH_ONE = BASE + "/{username}";
+    String FETCH_QUERY = BASE;
 //    String DELETE_BY_CODE = "/{code}";
 //    String UPDATE_NAME_BY_CODE = "/{code}/name";
 
