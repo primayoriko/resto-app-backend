0b261566beb85de15c9f24547f3d0d2626d2e950
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-05-08 07:58:21 +0700

    refactor: change error handling message with new dto

--- src/main/java/com/future/restoapp/controller/DefaultExceptionHandler.java
+++ src/main/java/com/future/restoapp/controller/DefaultExceptionHandler.java
@@ -1,19 +1,16 @@
 package com.future.restoapp.controller;
 
+import com.future.restoapp.model.dto.ErrorResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 //@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
@@ -23,29 +20,41 @@ public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
         super();
     }
 
+//    @Override
 //    @ExceptionHandler(value = { MethodArgumentNotValidException.class })
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+//            MethodArgumentNotValidException ex,
+//            HttpHeaders headers,
+//            HttpStatus status,
+//            WebRequest request
+//    ){
+//        ErrorResponse message = new ErrorResponse(
+//                status.value(),
+//                status.getReasonPhrase(),
+//                "Argument(s) passed is/are not valid",
+//                ""
+//        );
+//
+//        return ResponseEntity.badRequest().body(message);
+//    }
+
     @Override
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        Map<String, Object> body = new HashMap<>();
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request
+    ){
+        ErrorResponse message = new ErrorResponse(
+                status.value(),
+                status.getReasonPhrase(),
+                "Body Not Valid/Malformed",
+                ""
+        );
 
-        body.put("status", 400);
-        body.put("error", "BAD REQUEST");
-        body.put("message", "Argument(s) passed is/are not valid");
-
-        return ResponseEntity.badRequest().body(body);
-    }
-
-    @Override
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        Map<String, Object> body = new HashMap<>();
-
-        body.put("status", 400);
-        body.put("error", "BAD REQUEST");
-        body.put("message", "Body Not Valid/Malformed");
-
-        return ResponseEntity.badRequest().body(body);
+        return ResponseEntity.badRequest().body(message);
     }
 
     @Override
@@ -59,13 +68,16 @@ public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
             RuntimeException exception,
             WebRequest request
     ){
-        Map<String, Object> body = new HashMap<>();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        body.put("status", 500);
-        body.put("error", "INTERNAL SERVER ERROR");
-        body.put("message", "Unknown unhandled error, please contact the developer");
+        ErrorResponse message = new ErrorResponse(
+                status.value(),
+                status.getReasonPhrase(),
+                "Unknown unhandled error, please contact the developer",
+                ""
+        );
 
-        return ResponseEntity.status(500).body(body);
+        return ResponseEntity.badRequest().body(message);
     }
 
 }
---
+++ src/main/java/com/future/restoapp/model/dto/ErrorResponse.java
@@ -0,0 +1,26 @@
+package com.future.restoapp.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse implements Serializable {
+
+    private Integer status;
+
+    private String error;
+
+    private String message;
+
+    private String path;
+
+}
