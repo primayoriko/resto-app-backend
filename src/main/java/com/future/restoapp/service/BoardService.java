d302e50f7fbc91c0aaac919dba917585bc138a7d
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-05-31 13:26:19 +0700

    feat: add fetchAll and create board endpoint

--- src/main/java/com/future/restoapp/service/BoardService.java
+++ src/main/java/com/future/restoapp/service/BoardService.java
@@ -1,4 +1,18 @@
 package com.future.restoapp.service;
 
+import com.future.restoapp.model.entity.Board;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
 public interface BoardService {
+
+    Board create(Board board);
+
+    Collection<Board> findAll();
+
+    Collection<Board> findAllAvailable(LocalDateTime startTime, LocalDateTime endTime);
+
+    boolean checkAvailable(long id, LocalDateTime startTime, LocalDateTime endTime);
+
 }
--- src/main/java/com/future/restoapp/service/BoardServiceImpl.java
+++ src/main/java/com/future/restoapp/service/BoardServiceImpl.java
@@ -1,4 +1,37 @@
 package com.future.restoapp.service;
 
+import com.future.restoapp.model.entity.Board;
+import com.future.restoapp.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Service
 public class BoardServiceImpl implements BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Override
+    public Board create(Board board){
+        return boardRepository.save(board);
+    }
+
+    @Override
+    public Collection<Board> findAll(){
+        return boardRepository.findAll();
+    }
+
+    @Override
+    public Collection<Board> findAllAvailable(LocalDateTime startTime, LocalDateTime endTime){
+        return null;
+    }
+
+    @Override
+    public boolean checkAvailable(long id, LocalDateTime startTime, LocalDateTime endTime){
+        return true;
+    }
+
 }
