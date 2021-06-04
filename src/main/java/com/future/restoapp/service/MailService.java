4868e1f104af6f76d65769791b1e36f8ab296ba4
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-04 10:07:29 +0700

    feat: create mailing service

--- src/main/java/com/future/restoapp/service/MailService.java
+++ src/main/java/com/future/restoapp/service/MailService.java
@@ -1,8 +1,13 @@
 package com.future.restoapp.service;
 
+import javax.mail.MessagingException;
+import java.util.Map;
+
 public interface MailService {
 
-//    void sendMessageWithAttachment(String to, String subject,
-//                                   String text, String pathToAttachment) throws Exception;
+    void sendMessageWithTemplate(
+                String to, String subject,
+                Map<String, Object> templateModel
+        ) throws MessagingException;
 
 }
